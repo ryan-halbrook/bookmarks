97e748f4185ba5a1e5ebac41eee81d4c4bcf866a
ryan-halbrook <124092758+ryan-halbrook@users.noreply.github.com>
2023-06-08 16:50:23 -0700

    Adds CORS headers to topics blueprint

--- flaskr/topics.py
+++ flaskr/topics.py
@@ -4,6 +4,13 @@ import flaskr.core.topic as topic
 
 bp = Blueprint('topics', __name__, url_prefix='/')
 
+@bp.after_request
+def after_request(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'X-PINGOTHER, Content-Type'
+    response.headers['Access-Control-Allow-Methods'] = 'GET,POST,OPTIONS,DELETE,PATCH'
+    return response
+
 
 @bp.get('/topics')
 def topics():
