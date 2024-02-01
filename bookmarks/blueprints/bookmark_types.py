bf078bd866446b20f4b48905c1866b7f75edcc60
ryan-halbrook <124092758+ryan-halbrook@users.noreply.github.com>
2024-02-01 09:11:06 -0800

    Update README, add error checks

--- bookmarks/blueprints/bookmark_types.py
+++ bookmarks/blueprints/bookmark_types.py
@@ -9,6 +9,10 @@ bp = Blueprint('types', __name__, url_prefix='/')
 @bp.get('/collections/<id>/types')
 @login_required
 def types_get(id):
+    try:
+        id = int(id)
+    except ValueError:
+        abort(400)
     if collection.collection_user_id(id) != g.user.id:
         abort(404)
 
@@ -18,6 +22,11 @@ def types_get(id):
 @bp.patch('/types/<id>')
 @login_required
 def types_update(id):
+    try:
+        id = int(id)
+    except ValueError:
+        abort(400)
+
     data = request.json
     name = data.get('name', None)
     if not name:
@@ -35,6 +44,11 @@ def types_update(id):
 @bp.delete('/types/<id>')
 @login_required
 def types_delete(id):
+    try:
+        id = int(id)
+    except ValueError:
+        abort(400)
+
     btype = bookmark_type.fetch_single(id)
     if not btype:
         abort(404)
